e55d91af7563cb8049c7b1ee1bb5fbd1a50c911f
David Kolečkář <koleckar@protonmail.com>
2023-01-15 10:42:41 +0100

    Refactoring service logic to service layer. RestApiController.java refactored to more functional and readable code.

--- src/main/java/dk/EppTecMockApp/controller/RestApiController.java
+++ src/main/java/dk/EppTecMockApp/controller/RestApiController.java
@@ -1,9 +1,8 @@
 package dk.EppTecMockApp.controller;
 
 import dk.EppTecMockApp.dto.CustomerDto;
-import dk.EppTecMockApp.model.Customer;
 import dk.EppTecMockApp.model.CustomerRepository;
-import dk.EppTecMockApp.utils.Utils;
+import dk.EppTecMockApp.service.CustomerService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,57 +23,61 @@ public class RestApiController {
     private final Logger LOG;
 
     private final CustomerRepository customerRepository;
+    private final CustomerService customerService;
 
-    public RestApiController(CustomerRepository customerRepository) {
+    public RestApiController(CustomerRepository customerRepository, CustomerService customerService) {
         LOG = LoggerFactory.getLogger(RestApiController.class);
         this.customerRepository = customerRepository;
+        this.customerService = customerService;
     }
 
     @PostMapping
     public ResponseEntity<String> addCustomer(@RequestBody @Valid CustomerDto customerDto) {
-        LOG.info("POST " + customerDto);
-        customerRepository.save(new Customer(customerDto));
-        LOG.info("POST " + customerDto + " successfully added.");
+        customerService.saveCustomer(customerDto);
+        logRequest("POST", customerDto);
         return ResponseEntity.ok(customerDto + " successfully added.");
     }
 
-    //todo:
-    // nationalID jakarta.validation err not propagating.
+    // todo:
+    //  nationalID jakarta.validation err not propagating.
     @GetMapping
-    public ResponseEntity<Object> getCustomerByBirthCertificateNumber(@RequestParam("nationalID") String nationalID) {
-        LOG.info("POST " + nationalID);
-        Optional<Customer> customer = customerRepository.getCustomerByNationalID(
-                nationalID.replace("/", ""));
-
-        if (customer.isPresent()) {
-            return new ResponseEntity<>(
-                    Map.of("customer", new CustomerDto(customer.get()),
-                            "age", Utils.calculateAgeFromNationalID(nationalID)),
-                    HttpStatusCode.valueOf(200));
-        } else {
-            return new ResponseEntity<>(
-                    Map.of("customer", new CustomerDto(),
-                            "age", "null"),
-                    HttpStatusCode.valueOf(400));
-        }
+    public ResponseEntity<Map<String, Object>> getCustomer(@RequestParam("nationalID") String nationalID) {
+        Optional<CustomerDto> customer = customerService.getCustomerByNationalID(nationalID);
+        logRequest("GET", customer, nationalID);
+        return customer.map(
+                        val -> new ResponseEntity<>(
+                                Map.of("customer", val,
+                                        "age", customerService.getAge(nationalID)),
+                                HttpStatusCode.valueOf(200)))
+                .orElse(new ResponseEntity<>(
+                        Map.of("customer", new CustomerDto(),
+                                "age", "null"),
+                        HttpStatusCode.valueOf(400)));
     }
 
     @DeleteMapping
-    public ResponseEntity<Object> deleteCustomer(@RequestParam("nationalID") String nationalID) {
-        Optional<Customer> retrievedCustomer = customerRepository.getCustomerByNationalID(nationalID.replace("/", ""));
-        if (retrievedCustomer.isPresent()) {
-            Customer customer = retrievedCustomer.get();
-            customerRepository.deleteById(customer.getId());
-            LOG.info("deleted " + new CustomerDto(customer));
-            return new ResponseEntity<>(
-                    "Successfully deleted " + new CustomerDto(customer),
-                    HttpStatusCode.valueOf(200));
-        } else {
-            LOG.info("failed to delete customer with nationalID" + nationalID);
-            return new ResponseEntity<>(
-                    "Failed to delete Customer with nationalID=" + nationalID,
-                    HttpStatusCode.valueOf(400));
-        }
+    public ResponseEntity<Map<String, CustomerDto>> deleteCustomer(@RequestParam("nationalID") String nationalID) {
+        Optional<CustomerDto> customer = customerService.getCustomerByNationalID(nationalID);
+        customerService.deleteCustomerByNationalID(nationalID);
+        logRequest("DEL", customer, nationalID);
+
+        return customer.map(
+                        val -> new ResponseEntity<>(
+                                Map.of("customer", val),
+                                HttpStatusCode.valueOf(200)))
+                .orElse(new ResponseEntity<>(
+                        Map.of("customer", new CustomerDto()),
+                        HttpStatusCode.valueOf(400)));
     }
 
+    private void logRequest(String requestType, Optional<CustomerDto> customer, String nationalID) {
+        LOG.info(requestType);
+        customer.ifPresentOrElse(
+                val -> LOG.info(val + " success."),
+                () -> LOG.info("Customer with nationalID=" + nationalID + " not present"));
+    }
+
+    private void logRequest(String requestType, CustomerDto customer) {
+        LOG.info(requestType + " " + customer);
+    }
 }
---
+++ src/main/java/dk/EppTecMockApp/service/CustomerService.java
@@ -0,0 +1,40 @@
+package dk.EppTecMockApp.service;
+
+import dk.EppTecMockApp.dto.CustomerDto;
+import dk.EppTecMockApp.model.Customer;
+import dk.EppTecMockApp.model.CustomerRepository;
+import dk.EppTecMockApp.utils.Utils;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    private final CustomerRepository customerRepository;
+
+
+    public void saveCustomer(CustomerDto customerDto) {
+        customerRepository.save(new Customer(customerDto));
+    }
+
+    public Optional<CustomerDto> getCustomerByNationalID(String nationalID) {
+        nationalID = nationalID.replace("/", "");
+        return customerRepository.getCustomerByNationalID(nationalID)
+                .map(CustomerDto::new);
+    }
+
+    public void deleteCustomerByNationalID(String nationalID) {
+        nationalID = nationalID.replace("/", "");
+        Optional<Customer> retrievedCustomer = customerRepository.getCustomerByNationalID(nationalID);
+        retrievedCustomer.ifPresent(customer -> customerRepository.deleteById(customer.getId()));
+    }
+
+    public int getAge(String nationalID) {
+        return Utils.calculateAgeFromNationalID(nationalID);
+    }
+}
